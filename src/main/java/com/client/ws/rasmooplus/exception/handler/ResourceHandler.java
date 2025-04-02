565ca26b3261071cce44b54833581a318989cff3
Rafael Souza <rafaelsnt123@gmail.com>
2025-04-02 13:51:36 -0300

    Tratando mensagem de erro MethodArgumentNotValidException

--- src/main/java/com/client/ws/rasmooplus/exception/handler/ResourceHandler.java
+++ src/main/java/com/client/ws/rasmooplus/exception/handler/ResourceHandler.java
@@ -5,11 +5,36 @@ import com.client.ws.rasmooplus.exception.BadRequestException;
 import com.client.ws.rasmooplus.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class ResourceHandler {
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponseDto> methodArgumentNotValidException(MethodArgumentNotValidException m) {
+
+    Map<String, String> messages = new HashMap<>();
+
+    m.getBindingResult().getAllErrors().forEach(error -> {
+      String field = ((FieldError) error).getField();
+      String message = error.getDefaultMessage();
+
+      messages.put(field, message);
+    });
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponseDto.builder()
+        .message(Arrays.toString(messages.entrySet().toArray()))
+        .status(HttpStatus.BAD_REQUEST)
+        .statusCode(HttpStatus.BAD_REQUEST.value())
+        .build());
+  }
 
   @ExceptionHandler(NotFoundException.class)
   public ResponseEntity<ErrorResponseDto> notFoundException(NotFoundException n) {
